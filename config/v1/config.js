9ed61cd14fc8ec6653905e644c461185430b09db
Durga Parshad <durgadp16@gmail.com>
2022-05-05 12:53:17 +0530

    Update config.js

--- config/v1/config.js
+++ config/v1/config.js
@@ -6,8 +6,6 @@ let CONFIG = {};
 CONFIG.env = process.env.ENV || 'dev';
 CONFIG.port = process.env.PORT || '3000';
 
-CONFIG.unoapp_token = process.env.UNOAPP_TOKEN;
-
 CONFIG.mysql_driver = process.env.MYSQL_DRIVER || 'mysql';
 CONFIG.mysql_host = process.env.MYSQL_HOST || 'localhost';
 CONFIG.mysql_port = process.env.MYSQL_PORT || '3306';
@@ -21,11 +19,11 @@ CONFIG.redis_port = process.env.REDIS_PORT;
 CONFIG.api_ver = process.env.API_VER;
 
 if (process.env.ENV === 'prod' || process.env.ENV === 'production') {
-    CONFIG.cors_whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003', 'http://localhost:8000', 'http://localhost:8001', 'http://localhost:8002', 'http://localhost:8003', 'http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082', 'http://localhost:8083', 'http://localhost:6969', 'https://beta.freshii.com', 'https://www.freshii.com', 'https://freshii.com', 'https://freshii.typesift.com'];
+    CONFIG.cors_whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003', 'http://localhost:8000', 'http://localhost:8001', 'http://localhost:8002', 'http://localhost:8003', 'http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082', 'http://localhost:8083', 'http://localhost:6969'];
 } else if (process.env.ENV === 'stag' || process.env.ENV === 'staging') {
-    CONFIG.cors_whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003', 'http://localhost:8000', 'http://localhost:8001', 'http://localhost:8002', 'http://localhost:8003', 'http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082', 'http://localhost:8083', 'http://localhost:6969', 'https://beta.freshii.com', 'https://www.freshii.com', 'https://freshii.com', 'https://freshii.typesift.com']
+    CONFIG.cors_whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003', 'http://localhost:8000', 'http://localhost:8001', 'http://localhost:8002', 'http://localhost:8003', 'http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082', 'http://localhost:8083', 'http://localhost:6969']
 } else {
-    CONFIG.cors_whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003', 'http://localhost:8000', 'http://localhost:8001', 'http://localhost:8002', 'http://localhost:8003', 'http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082', 'http://localhost:8083', 'http://localhost:6969', 'https://beta.freshii.com', 'https://www.freshii.com', 'https://freshii.com', 'https://freshii.typesift.com']
+    CONFIG.cors_whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003', 'http://localhost:8000', 'http://localhost:8001', 'http://localhost:8002', 'http://localhost:8003', 'http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082', 'http://localhost:8083', 'http://localhost:6969']
 
     CONFIG.pn_publishKey = process.env.PUBNUB_PUBLISH;
     CONFIG.pn_subscribeKey = process.env.PUBNUB_SUBSCRIBE;
